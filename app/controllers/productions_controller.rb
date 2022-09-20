2295007527340f769e2e93fffa37af2b7dd22fc4
katyuya <93420930+katyuya@users.noreply.github.com>
2022-09-20 14:37:08 +0000

    [add]ライムラインボタンの色追加

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -4,7 +4,7 @@ class ProductionsController < ApplicationController
   end
 
   def create
-    @production = Production.new(invento_params)
+    @production = Production.new(production_params)
     if @production.save
       redirect_to productions_path
     else
@@ -23,7 +23,7 @@ class ProductionsController < ApplicationController
 
   def update
     @production = Production.find(params[:id])
-    if @production.update(invento_params)
+    if @production.update(production_params)
       redirect_to productions_path
     else
       render :edit
@@ -31,14 +31,14 @@ class ProductionsController < ApplicationController
   end
 
   def destroy
-    @production = Production.find(invento_params)
+    @production = Production.find(params[:id])
     @production.destroy
     redirect_to productions_path
   end
 
   private
 
-  def invento_params
+  def production_params
     params.require(:production).permit(:ordernumber, :customer, :product, :ruler, :platform, :progress)
   end
 end
